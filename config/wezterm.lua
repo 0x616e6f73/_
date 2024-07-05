70cbce879b7bf2c3d834859a7eac60d1e9328bae
0x616e6f73 <170067277+0x616e6f73@users.noreply.github.com>
2024-07-05 16:48:50 -0400

    messing w/ wezterm (decided against it)

--- config/wezterm.lua
+++ config/wezterm.lua
@@ -27,14 +27,14 @@ config.window_padding = {
 }
 
 wezterm.on("gui-startup", function(cmd)
-  if cmd == "helix" then
-    config.window_padding = {
-      left = 0,
-      right = 0,
-      top = 0,
-      bottom = 0,
-    }
-  end
+    if cmd == "helix" then
+        config.window_padding = {
+            left = 0,
+            right = 0,
+            top = 0,
+            bottom = 0,
+        }
+    end
 end)
 
 config.window_close_confirmation = "NeverPrompt"

974d15ba04d2bd232e1b45a697391a6fc508db26
0x616e6f73 <170067277+0x616e6f73@users.noreply.github.com>
2024-07-06 00:23:02 -0400

    fixing helix colors, organizing themes, fuck me

--- config/home.nix
+++ config/home.nix
@@ -3,16 +3,8 @@
   home.username = "ay";
   home.homeDirectory = lib.mkForce "/Users/ay";
   home.stateVersion = "23.11";
-
   fonts.fontconfig.enable = true;
   programs.home-manager.enable = true;
-
-  home.file.".config/wezterm/wezterm.lua".text = builtins.readFile ./wezterm.lua;
-
-  programs.bat = {
-    enable = true;
-    config.theme = "Flexoki";
-  };
 
   imports = [
     ./packages.nix
@@ -23,6 +15,13 @@
     ./tools/gpg.nix
     ./tools/ssh.nix
   ];
+
+  # home.file.".config/wezterm/wezterm.lua".text = builtins.readFile ./themes/wezterm.lua;
+
+  programs.bat = {
+    enable = true;
+    config.theme = "Flexoki";
+  };
 
   sops = {
     defaultSopsFile = ../secrets/git_config.yaml;

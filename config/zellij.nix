11a2e793d1ac56a6f3c34fbe14fdf47274533d9e
0x616e6f73 <170067277+0x616e6f73@users.noreply.github.com>
2024-07-07 14:08:14 -0400

    modified zellij tabs

--- config/zellij.nix
+++ config/zellij.nix
@@ -4,6 +4,24 @@
     settings = {
       pane_frames = false;
       theme = "ashys";
+      ui = {
+        pane_frames = {
+          rounded_corners = false;
+        };
+      };
+      session_name = "";
+      tab_bar_alignment = "center";
+      hide_session_name = true;
+      session_serialization = false;
+      simplified_ui = true;
+      default_layout = "compact";
+      default_mode = "normal";
+      mouse_mode = true;
+      scroll_buffer_size = 10000;
+      copy_command = "pbcopy";
+      copy_clipboard = "system";
+      copy_on_select = true;
+      scrollback_editor = "hx"; # Set to Helix
     };
   };
   home.file.".config/zellij/themes/ashys.yaml".source = ./themes/zellij.yaml;
